dcb9fd6e17d0bab44fe5eb7853a40e49ecef9c35
Nair <akash7nair@gmail.com>
2022-11-17 13:22:04 -0500

    pass data to the backend and get a response in the chrome extension

--- frontend/js/popup.js
+++ frontend/js/popup.js
@@ -1,81 +1,101 @@
 window.onload = (event) => {
 	const shiftColorBtn = document.querySelector("#colorShifter");
-	const spinner = document.querySelector("#spinner");
+	// const spinner = document.querySelector("#spinner");
 
-	chrome.storage.sync.get("COLOR_SHIFTED", ({ COLOR_SHIFTED }) => {
-		if (COLOR_SHIFTED) {
-			shiftColorBtn.textContent = "Color Shifted";
-			shiftColorBtn.classList.add("btn-danger");
-			shiftColorBtn.classList.remove("btn-primary");
-		} else {
-			shiftColorBtn.textContent = "Shift Color";
-			shiftColorBtn.classList.add("btn-primary");
-			shiftColorBtn.classList.remove("btn-danger");
-		}
-	});
+	// chrome.storage.sync.get("COLOR_SHIFTED", ({ COLOR_SHIFTED }) => {
+	// 	if (COLOR_SHIFTED) {
+	// 		shiftColorBtn.textContent = "Color Shifted";
+	// 		shiftColorBtn.classList.add("btn-danger");
+	// 		shiftColorBtn.classList.remove("btn-primary");
+	// 	} else {
+	// 		shiftColorBtn.textContent = "Shift Color";
+	// 		shiftColorBtn.classList.add("btn-primary");
+	// 		shiftColorBtn.classList.remove("btn-danger");
+	// 	}
+	// });
+
+
+	async function ajax_call_handler(method, endpoint, data) {
+
+        const response = await fetch(endpoint, {
+            method: method,
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: data
+        }).catch(error => {
+
+            return {
+                result: false
+            }
+
+        });
+
+        try {
+            return response.json();
+        } catch(err) {
+            return response;
+        }
+
+    }
 
 
 
 	shiftColorBtn.addEventListener("click", (event) => {
-		spinner.classList.remove("d-none");
-		chrome.storage.sync.get("COLOR_SHIFTED", ({ COLOR_SHIFTED }) => {
-			const colorShifted = !COLOR_SHIFTED;
-			chrome.storage.sync.set({ COLOR_SHIFTED: colorShifted }, () => {
-				if (colorShifted) {
-					shiftColorBtn.textContent = "Color Shifted";
-				} else {
-					shiftColorBtn.textContent = "Shift Color";
-				}
-				switchBtnState(shiftColorBtn);
-				spinner.classList.add("d-none");
-				spoofNavigator(colorShifted);
-			});
+		
+		var xyz = JSON.stringify({ x: 5, y: 6 });
+
+		ajax_call_handler("POST", "http://localhost:7000/images", xyz).then(resp => {
+			alert(resp.x);
 		});
+
 	});
 
 
-	async function getCurrentTabId() {
-		let [tab] = await chrome.tabs.query({
-			active: true,
-			currentWindow: true,
-		});
-		return tab.id;
-	}
+	// async function getCurrentTabId() {
+	// 	let [tab] = await chrome.tabs.query({
+	// 		active: true,
+	// 		currentWindow: true,
+	// 	});
+	// 	return tab.id;
+	// }
 
-	async function reload() {
-		const tabId = await getCurrentTabId();
-		chrome.tabs.reload(tabId, { bypassCache: true });
-	}
+	// async function reload() {
+	// 	const tabId = await getCurrentTabId();
+	// 	chrome.tabs.reload(tabId, { bypassCache: true });
+	// }
 
 
-	function switchBtnState(btn) {
-		btn.classList.toggle("btn-primary");
-		btn.classList.toggle("btn-danger");
-	}
+	// function switchBtnState(btn) {
+	// 	btn.classList.toggle("btn-primary");
+	// 	btn.classList.toggle("btn-danger");
+	// }
 
-	function spoofNavigator(colorShifted) {
-		const id = "inject_script_navigator";
-		if (colorShifted) {
-			chrome.scripting.registerContentScripts(
-				[
-					{
-						id,
-						matches: ["<all_urls>"],
-						allFrames: true,
-						runAt: "document_start",
-						js: ["js/colorShift.js"],
-					},
-				],
-				() => {
-					reload();
-				}
-			);
-		} else {
-			chrome.scripting.unregisterContentScripts({ ids: [id] }, () => {
-				reload();
-			});
-		}
-	}
+	// function spoofNavigator(colorShifted) {
+	// 	const id = "inject_script_navigator";
+	// 	if (colorShifted) {
+	// 		chrome.scripting.registerContentScripts(
+	// 			[
+	// 				{
+	// 					id,
+	// 					matches: ["<all_urls>"],
+	// 					allFrames: true,
+	// 					runAt: "document_start",
+	// 					js: ["js/colorShift.js"],
+	// 				},
+	// 			],
+	// 			() => {
+	// 				reload();
+	// 			}
+	// 		);
+	// 	} else {
+	// 		chrome.scripting.unregisterContentScripts({ ids: [id] }, () => {
+	// 			reload();
+	// 		});
+	// 	}
+	// }
 
 };
 
