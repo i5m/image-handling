b688672745517899b758f7bc8a43afcabe268bd7
Nair <akash7nair@gmail.com>
2022-12-05 12:58:02 -0500

    added 30 60 file for all three files

---
+++ frontend/src/protanopia_color_30_correcter.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    
+    var imgs = document.getElementsByTagName('img');
+    
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].src = `http://localhost:7000/images?extent=0.3&type_=prot&image_url=${imgs[i].src}`;
+    }
+    
+}, 2000);
